a185c22dfa66ce1d47ff1f45ddebc70ce5c7fa96
Nkepers <nick.kepers.coder@gmail.com>
2022-01-11 14:42:58 -0700

    updated schema and formatting

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const commentRoute = require('./comment-route');
 const pizzaRoute = require('./pizza-route');
 
-// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+router.use('/comments', commentRoute);
 router.use('/pizzas', pizzaRoute);
 
 module.exports = router;
